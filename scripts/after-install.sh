55e1b0ac03e04ddd4cf2896081b5e60794cfab2f
Anand Parmar <anand.parmar@traydo.com>
2023-05-14 12:48:18 +0530

    change

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -10,3 +10,12 @@ npm install pm2 -g
 cd /var/frontend/
 rm -r node_modules/
 yarn install
+
+pm2 describe app > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start yarn --name app -- start
+else
+  pm2 reload app
+fi;
